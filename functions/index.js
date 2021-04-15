e1c4590b0fe7b68d922eaa19543aa827914a6074
Dragan Markovic <dragan.markovic83@gmail.com>
2021-04-15 15:14:26 +0200

    test commit and push to github

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
 
+const test;
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
